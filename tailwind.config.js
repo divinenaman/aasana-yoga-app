14001375d8db0674f1eda21b52aa07b5d663e1e0
Naman Agarwal <naman.agarwal2019@vitstudent.ac.in>
2022-04-22 13:03:21 +0530

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}", "./App.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: require("tailwind-rn/unsupported-core-plugins"),
+};
